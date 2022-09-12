1b5407a98c03b0766afc4fd7d709d35bf7db7a6b
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-09-12 18:44:57 +0300

    rendering function

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
