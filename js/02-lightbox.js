c8ddc01dbe64d519eb0e0cc8289fd8794c8d86e3
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-09-13 20:42:43 +0300

    task 2 - simple lightbox libary

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// додаємо посилання на контейнер
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// створюємо розмітку картинки
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+// додаємо слухача на контейнер
+galleryContainer.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  // забороняємо браузеру переходити по посиланню
+  event.preventDefault();
+
+  // умова кліку - тільки по картинці (все інше ігнор)
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  //підключаємо бібліотеку
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
